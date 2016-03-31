1fbc2c036267d2193d1f5169820465b8d8197bd4
Satoshi Deguchi <deguchik@gmail.com>
2016-03-31 11:36:10 +0900

    change sideline to dateline

--- src/view/shelf.jsx
+++ src/view/shelf.jsx
@@ -1,24 +1,7 @@
 'use strict';
 
 import React from 'react'
-
-class AkebiSVGComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.svgs = [];
-  }
-  onClick() {
-    debug('click')
-  }
-  render() {
-    this.renderSVG();
-    return (
-      <g color="#D72541" onClick={this.onClick}>
-        {this.svgs}
-      </g>
-    )
-  }
-}
+import AkebiSVGComponent from './common.jsx'
 
 export default class Shelf extends AkebiSVGComponent {
   constructor(props) {
@@ -26,7 +9,7 @@ export default class Shelf extends AkebiSVGComponent {
     this.state = {
       "id": 4,
       "type": "shelf",
-      "side": 2,
+      "side": 1,
       "count": 8,
       "angle": 0,
       "top_cm": -99,
@@ -44,12 +27,18 @@ export default class Shelf extends AkebiSVGComponent {
   createShelf() {
     this.createRect();
     this.createPartitionLine();
-    this.createSide();
     if(this.state.side==1){
+      this.createSideLine();
     }
   }
   createRect() {
-    this.svgs.push(<rect x={this.x} y={this.y} width={this.width} height={this.height} stroke="currentColor" strokeWidth="1" fill="transparent"/>);
+    this.createVerticalLine(0);
+    this.createVerticalLine(this.state.count);
+    var x1 = this.x;
+    var y1 = this.y + this.height;
+    var x2 = this.x + this.width;
+    var y2 = this.y + this.height;
+    this.svgs.push(<line x1={x1} y1={y1} x2={x2} y2={y2} stroke="currentColor" strokeWidth="1"/>);
   }
   createPartitionLine() {
     this.createPartitionSideLine();
@@ -65,7 +54,7 @@ export default class Shelf extends AkebiSVGComponent {
   }
   createPartitionVerticalLine() {
     if(this.state.count<=1) return;
-    for(var i=1, l=this.state.count; i<=l; i++){
+    for(var i=1, l=this.state.count; i<l; i++){
       this.createVerticalLine(i);
     }
   }
@@ -76,11 +65,11 @@ export default class Shelf extends AkebiSVGComponent {
     var y2 = this.y + this.height;
     this.svgs.push(<line x1={x1} y1={y1} x2={x2} y2={y2} stroke="currentColor" strokeWidth="1"/>);
   }
-  createSide() {
+  createSideLine() {
     var x1 = this.x - 1;
-    var y1 = this.y + this.height;
+    var y1 = this.y;
     var x2 = this.x + this.width + 1;
-    var y2 = this.y + this.height;
-    this.svgs.push(<line x1={x1} y1={y1} x2={x2} y2={y2} stroke="currentColor" strokeWidth="5"/>);
+    var y2 = this.y;
+    this.svgs.push(<line x1={x1} y1={y1} x2={x2} y2={y2} stroke="currentColor" strokeWidth="1" strokeDasharray="4"/>);
   }
 }
