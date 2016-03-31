e2f2bc57813bb95ff2a17d6d557ac11b079c226c
Satoshi Deguchi <deguchik@gmail.com>
2016-03-31 12:11:56 +0900

    add strokeDash, add Point mode

--- src/view/svg/rect.jsx
+++ src/view/svg/rect.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import AkebiSVGComponent from './common.jsx'
+import Point from './point.jsx'
 
 export default class Rect extends AkebiSVGComponent {
   constructor(props) {
@@ -22,8 +23,28 @@ export default class Rect extends AkebiSVGComponent {
     this.y = 100;
     this.width = this.state.eachWidth * this.state.count;
     this.height = this.state.eachHeight * this.state.side;
+
   }
   renderSVG(){
-    this.svgs.push(<rect x={this.x} y={this.y} width={this.width} height={this.height} stroke="currentColor" strokeWidth="1" fill="transparent"/>);
+    // top line
+    var strokeDasharray = 0;
+    if(this.state.side==1){
+      strokeDasharray = 5;
+    }
+    this.drawLine(this.x, this.y, this.x + this.width, this.y, strokeDasharray);
+    // right line
+    this.drawLine(this.x + this.width, this.y, this.x + this.width, this.y + this.height);
+    // bottom line
+    this.drawLine(this.x + this.width, this.y + this.height, this.x, this.y + this.height);
+    // left line
+    this.drawLine(this.x, this.y + this.height, this.x, this.y);
+
+    //this.svgs.push(<rect x={this.x} y={this.y} width={this.width} height={this.height} stroke="currentColor" strokeWidth="1" fill="transparent"/>);
+  }
+  drawLine(x1, y1, x2, y2, strokeDasharray=0, drawPoint=true){
+    if(drawPoint){
+      this.svgs.push(<Point x={x1} y={y1}></Point>);
+    }
+    this.svgs.push(<line x1={x1} y1={y1} x2={x2} y2={y2} stroke="currentColor" strokeWidth="1" strokeDasharray={strokeDasharray}/>);
   }
 }
