730ed3ed1c896f37e98132a54ba8aacdb27e6029
Satoshi Deguchi <deguchik@gmail.com>
2016-04-07 12:03:14 +0900

    create Wall

--- src/component/Wall.jsx
+++ src/component/Wall.jsx
@@ -1,15 +1,25 @@
 'use strict';
 
+'use strict';
+
 import React from 'react';
 import AkebiSVGComponent from './AkebiSVGComponent.jsx'
+import Rect from './basic/Rect.jsx'
 
-export default class Wall extends AkebiSVGComponent {
+export default class Beacon extends AkebiSVGComponent {
   constructor(props) {
     super(props);
+    this.x = parseFloat(this.props.geojson.x) || 1;
+    this.y = parseFloat(this.props.geojson.y) || 1;
+    this.width = parseFloat(this.props.geojson.width) || 1;
+    this.height = parseFloat(this.props.geojson.height) || 1;
+    this.fill = this.props.fill || 'black';
+    this.stroke = this.props.stroke || 'black';
   }
   render(){
     return (
       <g>
+        <Rect x={this.x-this.width/2} y={this.y-this.height/2} width={this.width} height={this.height} fill={this.fill} strokeWidth="0" stroke={this.stroke}></Rect>
       </g>
     )
   }
